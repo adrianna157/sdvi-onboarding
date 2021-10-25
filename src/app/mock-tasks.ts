badde5fd6750dc631513ee3e155981197a3104ba
Adrianna Guevarra <adriannaestelleguevarra@gmail.com>
2021-10-25 11:32:06 -0700

    change logic for previous button

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -12,12 +12,12 @@ export const TASKS: CardInterface[] = [
     status: '50%',
     title: 'How To Request Equipment',
     statusBar: 'w-2/4',
-    main: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet doloremagna aliquam erat volutpat. Ut wisi enim ad minim veniam, quisnostrud exerci tation ullamcorper suscipit lobortis nisl utaliquip ex ea commodo consequat. Duis autem vel eum iriure dolorin hendrerit in vulputate velit esse molestie consequat, velillum dolore eu feugiat nulla facilisis at vero eros et accumsanet iusto odio dignissim qui blandit praesent luptatum zzrildelenit augue duis dolore te feugait nulla facilisi.',
+    main: 'Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Purus sit amet luctus venenatis. Duis ut diam quam nulla porttitor massa. Vestibulum lectus mauris ultrices eros in cursus turpis massa. Lectus proin nibh nisl condimentum id venenatis a condimentum vitae. Fermentum iaculis eu non diam phasellus vestibulum. Egestas maecenas pharetra convallis posuere morbi leo. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Aliquet nibh praesent tristique magna sit amet purus. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Dolor sit amet consectetur adipiscing elit pellentesque habitant. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam.',
     currentTask: 'Equipment Acquisition',
   },
   {
     status: '75%',
-    main: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet doloremagna aliquam erat volutpat. Ut wisi enim ad minim veniam, quisnostrud exerci tation ullamcorper suscipit lobortis nisl utaliquip ex ea commodo consequat. Duis autem vel eum iriure dolorin hendrerit in vulputate velit esse molestie consequat, velillum dolore eu feugiat nulla facilisis at vero eros et accumsanet iusto odio dignissim qui blandit praesent luptatum zzrildelenit augue duis dolore te feugait nulla facilisi.',
+    main: 'Quam quisque id diam vel. Est lorem ipsum dolor sit amet consectetur. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Erat imperdiet sed euismod nisi. Amet porttitor eget dolor morbi non arcu risus. In est ante in nibh mauris cursus mattis molestie a. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Sed cras ornare arcu dui vivamus. Purus in massa tempor nec feugiat nisl pretium. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Scelerisque varius morbi enim nunc faucibus a pellentesque. Diam ut venenatis tellus in metus.',
     statusBar: 'w-3/4',
     title: 'How To Setup Your Environment',
     currentTask: 'Environment Setup',
@@ -26,7 +26,7 @@ export const TASKS: CardInterface[] = [
     status: '100%',
     title: 'Required Trainings',
     statusBar: 'w-4/4',
-    main: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet doloremagna aliquam erat volutpat. Ut wisi enim ad minim veniam, quisnostrud exerci tation ullamcorper suscipit lobortis nisl utaliquip ex ea commodo consequat. Duis autem vel eum iriure dolorin hendrerit in vulputate velit esse molestie consequat, velillum dolore eu feugiat nulla facilisis at vero eros et accumsanet iusto odio dignissim qui blandit praesent luptatum zzrildelenit augue duis dolore te feugait nulla facilisi.',
+    main: 'Risus ultricies tristique nulla aliquet enim tortor at auctor. Nulla facilisi etiam dignissim diam quis enim. Sit amet risus nullam eget felis eget nunc. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Eu volutpat odio facilisis mauris sit amet massa. Tortor vitae purus faucibus ornare suspendisse. At augue eget arcu dictum. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Sit amet justo donec enim diam. Mauris augue neque gravida in. Rhoncus urna neque viverra justo nec ultrices dui sapien eget. Quisque egestas diam in arcu cursus euismod quis viverra. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Est sit amet facilisis magna etiam tempor orci eu. At ultrices mi tempus imperdiet nulla malesuada pellentesque. Posuere urna nec tincidunt praesent semper.',
     currentTask: 'Individualized Team Training',
   },
 ];
