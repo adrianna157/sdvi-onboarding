5daebc74eead5ffcadb9fe71ba2542eb01bfa714
Adrianna Guevarra <adriannaestelleguevarra@gmail.com>
2021-10-25 20:13:16 -0700

    match the tech blue that is on SDVI website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-    prefix: '',
-    purge: {
-      content: [
-        './src/**/*.{html,ts}',
-      ]
+  prefix: "",
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        techBlue: {
+          DEFAULT: "#3DB7E4",
+        },
+      },
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
-    prefix: '',
-        purge: {
-        enabled: true,
-        content: [
-            './src/**/*.{html,ts}',
-        ]
-    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  prefix: "",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
 };
